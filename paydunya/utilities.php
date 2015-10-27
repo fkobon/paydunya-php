2478b5774ecc7716e7e93ff9f4b0c9db37f7165a
Honore Hounwanou <mercuryseries@gmail.com>
2015-10-27 01:57:30 +0100

    Initial commit

---
+++ paydunya/utilities.php
@@ -0,0 +1,61 @@
+<?php
+class Paydunya_Utilities {
+
+  // prevent instantiation of this class
+  private function __construct() {}
+
+  public static function httpJsonRequest($url,$data=array()){
+    Requests::register_autoloader();
+    $headers = array(
+      'Accept' => 'application/json',
+      'PAYDUNYA-PUBLIC-KEY' => Paydunya_Setup::getPublicKey(),
+      'PAYDUNYA-PRIVATE-KEY' => Paydunya_Setup::getPrivateKey(),
+      'PAYDUNYA-MASTER-KEY' => Paydunya_Setup::getMasterKey(),
+      'PAYDUNYA-TOKEN' => Paydunya_Setup::getToken(),
+      'PAYDUNYA-MODE' => Paydunya_Setup::getMode(),
+      'User-Agent' => "PAYDUNYA Checkout API PHP client v1 aka Neptune"
+    );
+    if (JSON_ENCODE_PARAM_SUPPORT) {
+      $json_payload = json_encode($data,JSON_FORCE_OBJECT);
+    }else{
+      $json_payload = json_encode((object)$data);
+    }
+
+    $request = Requests::post($url, $headers,$json_payload,array('timeout' => 10));
+
+    return json_decode($request->body,true);
+  }
+
+  public static function httpPostRequest($url,$data=array()){
+    Requests::register_autoloader();
+    $headers = array(
+      'Accept' => 'application/x-www-form-urlencoded',
+      'PAYDUNYA-PUBLIC-KEY' => Paydunya_Setup::getPublicKey(),
+      'PAYDUNYA-PRIVATE-KEY' => Paydunya_Setup::getPrivateKey(),
+      'PAYDUNYA-MASTER-KEY' => Paydunya_Setup::getMasterKey(),
+      'PAYDUNYA-TOKEN' => Paydunya_Setup::getToken(),
+      'PAYDUNYA-MODE' => Paydunya_Setup::getMode(),
+      'User-Agent' => "PAYDUNYA Checkout API PHP client v1 aka Neptune"
+    );
+
+    $request = Requests::post($url, $headers,$data,array('timeout' => 10));
+
+    return json_decode($request->body,true);
+  }
+
+  public static function httpGetRequest($url){
+    Requests::register_autoloader();
+    $headers = array(
+      'PAYDUNYA-PUBLIC-KEY' => Paydunya_Setup::getPublicKey(),
+      'PAYDUNYA-PRIVATE-KEY' => Paydunya_Setup::getPrivateKey(),
+      'PAYDUNYA-MASTER-KEY' => Paydunya_Setup::getMasterKey(),
+      'PAYDUNYA-TOKEN' => Paydunya_Setup::getToken(),
+      'PAYDUNYA-MODE' => Paydunya_Setup::getMode(),
+      'User-Agent' => "PAYDUNYA Checkout API PHP client v1 aka Neptune"
+    );
+
+    $request = Requests::get($url, $headers,array('timeout' => 10));
+
+    return json_decode($request->body,true);
+  }
+}
